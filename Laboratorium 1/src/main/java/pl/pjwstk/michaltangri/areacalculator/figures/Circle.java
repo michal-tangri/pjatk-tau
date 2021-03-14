5a2a6908508434e49bad16982c2b9be0a51c9453
Michal Tangri <mt.michaltangri@gmail.com>
2021-03-14 15:29:36 +0100

    Add first exercise with 10 tests

---
+++ Laboratorium 1/src/main/java/pl/pjwstk/michaltangri/areacalculator/figures/Circle.java
@@ -0,0 +1,46 @@
+package pl.pjwstk.michaltangri.areacalculator.figures;
+
+public class Circle implements Figure {
+
+    private double r;
+    private String name = null;
+
+    public Circle() {
+        this.r = 1;
+    }
+
+    public Circle(final double r) throws IllegalArgumentException{
+        if (r <= 0) throw new IllegalArgumentException("Radius cannot be 0 or less.");
+        this.r = r;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * (r * r);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * r;
+    }
+
+    public static boolean isCircle(final Figure figure) {
+        return figure instanceof Circle;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
